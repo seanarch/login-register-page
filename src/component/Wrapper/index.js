067226df59efb5af787ed5283aa59d052e01c316
Sean <seanzzz@Seans-Mac-Studio.local>
2023-03-23 11:42:39 -0600

    login and registration form

--- src/component/Wrapper/index.js
+++ src/component/Wrapper/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import "./_wrapper.scss";
-// import { IonIcon, addIcons } from "@ionic/react";
-// import { lockClosedOutline, mailOutline } from "ionicons";
+import { IonIcon } from "@ionic/react";
+import {
+  lockClosedOutline,
+  mailOutline,
+  closeOutline,
+  personOutline,
+} from "ionicons/icons";
 
 export default function Wrapper() {
   return (
     <div className="wrapper">
+      <span className="icon-close">
+        <IonIcon icon={closeOutline} />
+      </span>
       <div className="form-box login">
         <h2>Login</h2>
         <form action="#">
           <div className="input-box">
-            <span className="icon"></span>
+            <span className="icon">
+              <IonIcon icon={mailOutline} />
+            </span>
             <input type="email" required />
             <label>Email</label>
           </div>
           <div className="input-box">
-            <span className="icon"></span>
+            <span className="icon">
+              <IonIcon icon={lockClosedOutline} />
+            </span>
             <input type="password" required />
             <label>Password</label>
           </div>
@@ -25,17 +37,61 @@ export default function Wrapper() {
               Remember me
             </label>
             <a href="#">Forgot Password?</a>
-            <button type="submit" className="btn">
-              Login
-            </button>
-            <div className="login-register">
-              <p>
-                Don't have an account?
-                <a href="#" className="register-link">
-                  Register
-                </a>
-              </p>
-            </div>
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+          <div className="login-register">
+            <p>
+              Don't have an account?
+              <a href="#" className="register-link">
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+
+      <div className="form-box register">
+        <h2>Registration</h2>
+        <form action="#">
+          <div className="input-box">
+            <span className="icon">
+              <IonIcon icon={personOutline} />
+            </span>
+            <input type="text" required />
+            <label>Username</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <IonIcon icon={mailOutline} />
+            </span>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+
+          <div className="input-box">
+            <span className="icon">
+              <IonIcon icon={lockClosedOutline} />
+            </span>
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />I agree to the terms & conditions
+            </label>
+          </div>
+          <button type="submit" className="btn">
+            Register
+          </button>
+          <div className="login-register">
+            <p>
+              Already have an account?
+              <a href="#" className="login-link">
+                Login
+              </a>
+            </p>
           </div>
         </form>
       </div>
